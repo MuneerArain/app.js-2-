22d9eec4fe7f26868afc32656e9c31fbb18d9775
MuneerArain <muneerarain115@gmail.com>
2025-08-23 11:54:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+console.log("JavaScript Connected")
+
+// chapter: Arrays
+
+
+// var studentNames = [];
+
+
+// var studentNames = Array();
+
+
+// var fruits = ["Apple", "Banana", "Mango", "Orange"];
+// console.log(fruits[0]); // Output: Apple
+
+// var numbers = ["1","2","3","4","5"];
+// console.log(numbers[0]);
+
+// var numbers = ["true","false"]
+// console.log(numbers[1]);
+
+// var mixedArray = ["Ali", 25, true, null, undefined, { city: "Lahore" }, [1, 2, 3]];
+// console.log(mixedArray[6])
+
+//  var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+//  document.write(qualifications );
+
+//         var studentNames = ["Ali", "Fatima", "Ahmed"];
+//         var studentScores = [410, 375, 450];
+
+//         // Total marks
+//         var totalMarks = 500;
+
+//         // Display the result
+//         for (var i = 0; i < studentNames.length; i++) {
+//             var percentage = (studentScores[i] / totalMarks) * 100;
+//             document.write("Score of " + studentNames[i] + " is " + studentScores[i] + 
+//                            ". Percentage: " + percentage.toFixed(2) + "<br>");
+//         }
+
+
+    
+
+
+
+
+
+
+    
